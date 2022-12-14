41e5ea28fc21f354ebd47a9fed0bc54438afea7a
Yuki Ito <37338201+ykit00@users.noreply.github.com>
2022-12-15 04:21:44 +0900

    add and init next-firebase-auth

---
+++ utils/initAuth.js
@@ -0,0 +1,60 @@
+import { init } from 'next-firebase-auth'
+
+const initAuth = () => {
+  init({
+    authPageURL: '/auth',
+    appPageURL: '/',
+    loginAPIEndpoint: '/api/login', // required
+    logoutAPIEndpoint: '/api/logout', // required
+    onLoginRequestError: (err) => {
+      console.error(err)
+    },
+    onLogoutRequestError: (err) => {
+      console.error(err)
+    },
+    firebaseAuthEmulatorHost: 'localhost:9099',
+    firebaseAdminInitConfig: {
+      credential: {
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        // The private key must not be accessible on the client side.
+        privateKey: process.env.GOOGLE_APPLICATION_CREDENTIALS
+          ? JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS).private_key
+          : undefined,
+      },
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    },
+    // Use application default credentials (takes precedence over firebaseAdminInitConfig if set)
+    // useFirebaseAdminDefaultCredential: true,
+    firebaseClientInitConfig: {
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    },
+    cookies: {
+      name: 'try-next-firebase-auth', // required
+      // Keys are required unless you set `signed` to `false`.
+      // The keys cannot be accessible on the client side.
+      keys: [
+        process.env.COOKIE_SECRET_CURRENT,
+        process.env.COOKIE_SECRET_PREVIOUS,
+      ],
+      httpOnly: true,
+      maxAge: 12 * 60 * 60 * 24 * 1000, // twelve days
+      overwrite: true,
+      path: '/',
+      sameSite: 'strict',
+      secure: true, // set this to false in local (non-HTTPS) development
+      signed: true,
+    },
+    onVerifyTokenError: (err) => {
+      console.error(err)
+    },
+    onTokenRefreshError: (err) => {
+      console.error(err)
+    },
+  })
+}
+
+export default initAuth
